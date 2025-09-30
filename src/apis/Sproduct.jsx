f7277698322078bc5492831430a6786d452184e0
oshaq naimat <oshaqnaimat15@gmail.com>
2025-09-30 08:27:03 +0500

    second api complete

--- src/apis/Sproduct.jsx
+++ src/apis/Sproduct.jsx
@@ -1,17 +1,38 @@
+// import React from 'react'
+
+// const Sproduct = ({images,brand,title,price}) => {
+//   return (
+//     <>
+//     <div className="container shadow-lg shadow-black  rounded-md">
+//         <img src={images[0]} alt="" />
+//         <h2 className='text-center font-semibold'>{title}</h2>
+//         <div className="flex justify-around">
+//         <p className='text-gray-800'>{brand}</p>
+//         <p className='text-gray-800'>{price}</p>
+//         </div>
+//     </div>
+    
+//     </>
+//   )
+// }
+
+// export default Sproduct
+
 import React from 'react'
 
 const Sproduct = ({images,brand,title,price}) => {
   return (
     <>
-    <div className="container shadow-lg shadow-black  rounded-md">
-        <img src={images[0]} alt="" />
-        <h2 className='text-center font-semibold'>{title}</h2>
-        <div className="flex justify-around">
-        <p className='text-gray-800'>{brand}</p>
-        <p className='text-gray-800'>{price}</p>
-        </div>
-    </div>
     
+    <div className="container shadow-lg shadow-black rounded-md">
+      <img src={images}  alt="" />
+
+      <h2 className='text-cneter text-2xl font-semibold'>{title}</h2>
+      <div className="flex justify-around">
+        <p className='text-gray-800 text-sm'>{brand}</p>
+        <p className='text-gray-800 text-sm'>{price}</p>
+      </div>
+    </div>
     </>
   )
 }
