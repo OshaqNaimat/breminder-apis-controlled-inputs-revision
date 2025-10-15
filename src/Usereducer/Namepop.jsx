fc311f7f5c3909018244e427a7067f8152bc9bb5
oshaq naimat <oshaqnaimat15@gmail.com>
2025-10-15 19:06:36 +0500

    eight commit

--- src/Usereducer/Namepop.jsx
+++ src/Usereducer/Namepop.jsx
@@ -15,13 +15,13 @@
 import React from 'react'
 import { IoClose } from 'react-icons/io5'
 
-const Namepop = () => {
+const Namepop = ({name,setOpen}) => {
   return (
     <>
     <div className="min-h-screen bg-black/50 backdrop-blur w-full fixed text-5xl top-0 start-0
     flex justify-center items-center">
-    <h1 className='bg-white rounded-md p-10'>Name</h1>
-    <IoClose className='absolute top-5 right-5 text-white cursor-pointer '/>
+    <h1 className='bg-white rounded-md p-10'>{name}</h1>
+    <IoClose onClick={()=>setOpen(false)} className='absolute top-5 right-5 text-white cursor-pointer '/>
     </div>
     </>
   )
