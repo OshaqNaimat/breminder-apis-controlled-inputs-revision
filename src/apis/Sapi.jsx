9f47c692ced34d42b0a718da2c80bbb16aeaa64b
oshaq naimat <oshaqnaimat15@gmail.com>
2025-10-16 19:53:21 +0500

    Sjson

--- src/apis/Sapi.jsx
+++ src/apis/Sapi.jsx
@@ -50,7 +50,7 @@ const Sapi = () => {
     <h1 className='text-3xl font-semibold text-center'>Products</h1>
     <div className="contianer grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
       {products?.map((item,index)=>{
-        
+        return <Sproduct/>
       })}
     </div>
     </>
--- src/apis/Sproduct.jsx
+++ src/apis/Sproduct.jsx
@@ -23,14 +23,14 @@ import React from 'react'
 const Sproduct = ({images,brand,title,price}) => {
   return (
     <>
-    
     <div className="container shadow-lg shadow-black rounded-md">
-      <img src={images[0]}  alt="" />
-
-      <h2 className='text-center text-xl font-semibold'>{title}</h2>
+      <img src={images[0]} alt="" />
+      <h2 className='text-center font-semibold'>
+        {title}
+      </h2>
       <div className="flex justify-around">
-        <p className='text-gray-800 text-sm'>{brand}</p>
-        <p className='text-gray-800 text-sm'>{price}</p>
+        <p className='text-gray-800'>{brand}</p>
+        <p className='text-gray-800'>{price}</p>
       </div>
     </div>
     </>
